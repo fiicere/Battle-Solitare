3c1fdb49239d27529a9280414263ae7ccbbf933f
fiicere <fiicere@gmail.com>
2014-10-23 22:37:29 -0400

    Revert 39c6718..abab500
    This rolls back to commit 39c671847c574ab3bbda3e1c701a257b57160e12.

--- Battle Solitare/CardTutorialLayer.h
+++ Battle Solitare/CardTutorialLayer.h
@@ -8,9 +8,9 @@
 
 #import <Foundation/Foundation.h>
 #import "cocos2d.h"
-#import "TextWithRects.h"
+#import "TitleWithRects.h"
 
-@interface CardTutorialLayer :  TextWithRects{
+@interface CardTutorialLayer :  TitleWithRects{
     
 }
 
--- Battle Solitare/TitleWithRects.h
+++ Battle Solitare/TitleWithRects.h
@@ -10,7 +10,7 @@
 #import "cocos2d.h"
 #import "BackgroundWithRects.h"
 
-@interface TextWithRects : BackgroundWithRects {
+@interface TitleWithRects : BackgroundWithRects {
     
 }
 
